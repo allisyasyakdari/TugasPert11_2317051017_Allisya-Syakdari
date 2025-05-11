a91083f50b22013aa27bb39eba8bc4b7e8b11719
allisyasyakdari <allisyasyakdarisdn@gmail.com>
2025-05-11 23:41:35 +0700

    Simpan perubahan sebelum rebase

--- form login/login.php
+++ form login/login.php
@@ -1,3 +1,4 @@
+
 <?php
 session_start();
 include 'koneksi/db.php';
@@ -7,9 +8,10 @@ $query = "SELECT * FROM users WHERE username='$username'";
 $result = mysqli_query($conn, $query);
 $user = mysqli_fetch_assoc($result);
 if ($user && password_verify($password, $user['password'])) {
-$_SESSION['user'] = $user['username'];
-header("Location: dashboard.php");
+    $_SESSION['user'] = $user['username'];
+    header("Location: dashboard.php");
 } else {
-echo "Login gagal. Username atau password salah.";
+    header("Location: index.php?error=1");
+    exit;
 }
 ?>
